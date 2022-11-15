3503571a3a7f60916aecf89f7905d983a86b0c6d
Bo0oqka <yamshhikova98@list.ru>
2022-11-16 01:21:56 +0700

    #1 / Added a list of facts about cats, also added pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import {Layout} from 'antd';
+import React, {useState} from 'react';
+import {Pagination} from 'antd';
 import './App.css';
+import {useQuery} from "@tanstack/react-query";
 
-function App() {
+const {Header, Content, Footer} = Layout;
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
+
+  const onChange = (page, pageSize) => {
+    setCurrentPage(page);
+    setCurrentPageSize(pageSize);
+  };
+
+  const {isLoading, error, data} = useQuery({
+    queryKey: ['repoData', currentPage, currentPageSize],
+    keepPreviousData : true,
+    queryFn: () =>
+      fetch(`https://catfact.ninja/facts?page=${currentPage}&limit=${currentPageSize}`).then(res =>
+        res.json()
+      )
+  })
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+    <Layout className="layout">
+      <Header>
+        <div className="logo"/>
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <div className="site-layout-content">
+          {
+            isLoading
+              ? 'Loading...'
+              : data.data.map((item, index) => {
+                return (
+                  <div key={index} className="cats-item">
+                    <div>
+                      {item.fact}
+                    </div>
+                    <div>
+                      {item.length}
+                    </div>
+                  </div>
+                )
+              })}
+          <Pagination current={currentPage} showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+                      onChange={onChange} total={data?.total} />
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  )
+};
 export default App;
